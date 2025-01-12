8af5cbef99c75fcabda27a6bb9401552bcf0109a
SercanSever <sercan.sever16@gmail.com>
2025-01-12 11:53:45 +0300

    add signup functionality with user avatar upload; update login component and Cloudinary integration

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import "./login.css";
-import { UploadCloudinary } from "../../lib/upload-cloudinary";
+import { toast } from "react-toastify";
+import { uploadCloudinary } from "../../lib/upload-cloudinary";
+import { auth, db } from "../../lib/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 const Login = () => {
   const [userAvatar, setUserAvatar] = useState({
+    file: null,
     url: "",
   });
 
   const handleUserAvatar = async (e) => {
     if (!e.target.files[0]) return;
     const file = e.target.files[0];
-    const res = await UploadCloudinary({ file });
-    setUserAvatar({ url: res });
+    setUserAvatar({ file, url: URL.createObjectURL(file) });
   };
 
   const handleLogin = (e) => {
@@ -19,6 +23,47 @@ const Login = () => {
     var form = new FormData(e.target);
     var { email, password } = Object.fromEntries(form);
     console.log(email, password);
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    var form = new FormData(e.target);
+    var { username, email, password } = Object.fromEntries(form);
+
+    try {
+      const userAuthCredentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const cloudinaryResponse = await uploadCloudinary({
+        file: userAvatar.file,
+        userId: userAuthCredentials.user.uid,
+      });
+
+      await setDoc(doc(db, "users", userAuthCredentials.user.uid), {
+        id: userAuthCredentials.user.uid,
+        username,
+        email,
+        avatar: cloudinaryResponse,
+        blocked: [],
+      });
+
+      await setDoc(doc(db, "userChats", userAuthCredentials.user.uid), {
+        chats: [],
+      });
+
+      toast.success("User Created Successfully");
+    } catch (error) {
+      console.error(error);
+      if (error.code === "auth/email-already-in-use")
+        toast.error("Email Already In Use");
+      if (error.code === "auth/weak-password") toast.error("Weak Password");
+      if (error.code === "auth/invalid-email") toast.error("Invalid Email");
+      if (error.code === "auth/operation-not-allowed")
+        toast.error("Operation Not Allowed");
+    }
   };
 
   return (
@@ -34,7 +79,7 @@ const Login = () => {
       <div className="seperator"></div>
       <div className="item">
         <h2>Create an Account</h2>
-        <form action="">
+        <form onSubmit={handleSignup}>
           <label htmlFor="file">
             <img src={userAvatar.url || "/avatar.png"} alt="" />
             <p style={userAvatar.url !== "" ? { display: "none" } : {}}>
--- src/lib/upload-cloudinary.jsx
+++ src/lib/upload-cloudinary.jsx
@@ -1,11 +1,14 @@
-export const UploadCloudinary = async ({ file }) => {
+export const uploadCloudinary = async ({ file, userId }) => {
   const formData = new FormData();
   formData.append("file", file);
   formData.append(
     "upload_preset",
     import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
   );
-  formData.append("folder", import.meta.env.VITE_CLOUDINARY_FOLDER);
+  formData.append(
+    "folder",
+    `${import.meta.env.VITE_CLOUDINARY_FOLDER}-${userId}`
+  );
 
   try {
     const response = await fetch(import.meta.env.VITE_CLOUDINARY_UPLOAD_URL, {
@@ -23,6 +26,33 @@ export const UploadCloudinary = async ({ file }) => {
     return data.secure_url;
   } catch (error) {
     console.error("Yükleme Hatası:", error);
+    throw error;
+  }
+};
+
+export const removeFolder = async (folder) => {
+  const formData = new FormData();
+  formData.append("folder", folder);
+
+  try {
+    const response = await fetch(
+      import.meta.env.VITE_CLOUDINARY_DELETE_FOLDER_URL,
+      {
+        method: "DELETE",
+        body: formData,
+      }
+    );
+
+    if (!response.ok) {
+      const error = await response.json();
+      console.error("Cloudinary Hatası:", error);
+      throw new Error(error.message || "Silme Hatası");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Silme Hatası:", error);
     throw error;
   }
 };
