3557013293f35855f35b883333bc2b7f8d170194
SercanSever <sercan.sever16@gmail.com>
2025-01-20 19:39:26 +0300

    feat: add delete account functionality and enhance user info dropdown menu

--- src/components/list/user-info/user-info.jsx
+++ src/components/list/user-info/user-info.jsx
@@ -8,8 +8,11 @@ const UserInfo = () => {
   const { currentUser } = useUserStore();
   const { chatId } = useChatStore();
   const inputRef = useRef(null);
+  const dropDownRef = useRef(null);
+  const menuRef = useRef(null);
   const [nameChangeControl, setNameChangeControl] = useState(false);
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+  const [deleteAccountPopup, setDeleteAccountPopup] = useState(false);
 
   const handleNameChange = () => {
     setNameChangeControl(!nameChangeControl);
@@ -34,63 +37,94 @@ const UserInfo = () => {
     setIsDropdownVisible((prev) => !prev);
   };
 
+  const handleDeleteAccount = () => {
+    setDeleteAccountPopup(true);
+  };
+
   useEffect(() => {
-    if (nameChangeControl && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [nameChangeControl]);
+    const handleClickOutside = (e) => {
+      if (
+        dropDownRef.current &&
+        !dropDownRef.current.contains(e.target) &&
+        menuRef.current &&
+        !menuRef.current.contains(e.target)
+      ) {
+        setIsDropdownVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className="userInfo">
-      <div className="user">
-        <img
-          src={currentUser.avatar || "/chat-rq-logo-background.png"}
-          alt=""
-        />
-        {!nameChangeControl ? (
-          <h4>{currentUser.username}</h4>
-        ) : (
-          <input
-            type="text"
-            ref={inputRef}
-            defaultValue={currentUser.username}
-          />
-        )}
-        {!chatId && (
-          <button className="logout" onClick={() => auth.signOut()}>
-            <img src="/switch.png" alt="" />
-          </button>
-        )}
-      </div>
-      <div className="icons">
-        {!nameChangeControl ? (
-          <>
-            <img
-              src="/more.png"
-              alt=""
-              onClick={toggleDropdown}
-              className="dropdownIcon"
-            />
-            {isDropdownVisible && (
-              <div className="dropdownMenu">
-                <ul>
-                  <li>Option 1</li>
-                  <li>Option 2</li>
-                  <li onClick={() => auth.signOut()}>Logout</li>
-                </ul>
-              </div>
-            )}
-            <img src="/edit.png" alt="" onClick={handleNameChange} />
-          </>
-        ) : (
+      {deleteAccountPopup && (
+        <div className="deleteAccount">
+          <p>Are you sure ?</p>
+          <div className="deleteIcons">
+            <img src="/letter-x.png" alt="" />
+            <img src="/check.png" alt="" />
+          </div>
+        </div>
+      )}
+      {!deleteAccountPopup && (
+        <div className="user">
           <img
-            src="/check.png"
+            src={currentUser.avatar || "/chat-rq-logo-background.png"}
             alt=""
-            className="yesIcon"
-            onClick={handleUserNameUpdate}
           />
-        )}
-      </div>
+
+          {!nameChangeControl ? (
+            <h4>{currentUser.username}</h4>
+          ) : (
+            <input
+              type="text"
+              ref={inputRef}
+              defaultValue={currentUser.username}
+            />
+          )}
+          {!chatId && (
+            <button className="logout" onClick={() => auth.signOut()}>
+              <img src="/switch.png" alt="" />
+            </button>
+          )}
+        </div>
+      )}
+      {!deleteAccountPopup && (
+        <div className="icons">
+          {!nameChangeControl ? (
+            <>
+              <img
+                src="/more.png"
+                alt=""
+                onClick={toggleDropdown}
+                className="dropdownIcon"
+                ref={dropDownRef}
+              />
+              {isDropdownVisible && (
+                <div className="dropdownMenu" ref={menuRef}>
+                  <button>Edit Status</button>
+                  <button onClick={handleDeleteAccount} className="logout">
+                    Delete Account
+                  </button>
+                  <button onClick={() => auth.signOut()}>Logout</button>
+                </div>
+              )}
+              <img src="/edit.png" alt="" onClick={handleNameChange} />
+            </>
+          ) : (
+            <img
+              src="/check.png"
+              alt=""
+              className="yesIcon"
+              onClick={handleUserNameUpdate}
+            />
+          )}
+        </div>
+      )}
     </div>
   );
 };
